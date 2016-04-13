ca7187f74b004da2210a92b3b4aefb504fd43cbf
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-04-13 23:39:39 +0200

    Initial core and course components.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # dyb-components
 DYB components for LMS
+
+DYB components is a suite of Learning Management System Components and Bundles built on top of Symfony 
+and under [MIT](http://opensource.org/licenses/MIT) license.
+It aims to promote SOLID principles, efficient code reuse, separation of 
+concerns as effective building blocks for the development of LMS 
+applications.
+
+DYB provides a reference implementation for the basic core components found 
+in LMS web projects.
+
+Requirements
+------------
+
+Elcodi is supported on PHP 5.5.* and up.
---
+++ src/DYB/Component/Core/Entity/Traits/IdentifiableTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the DYB package.
+ *
+ * Copyright (c) 2016 AHDO Studio B.V.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Arkaitz Garro <arkaitz.garro@gmail.com>
+ */
+
+/**
+ * Trait IdentifiableTrait.
+ */
+trait IdentifiableTrait
+{
+    /**
+     * @var int
+     *
+     * Identifier
+     */
+    protected $id;
+
+    /**
+     * Get Id.
+     *
+     * @return int Id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Sets Id.
+     *
+     * @param int $id Id
+     *
+     * @return $this Self object
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
