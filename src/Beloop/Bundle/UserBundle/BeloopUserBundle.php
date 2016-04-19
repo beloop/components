2a94539b52f7d0ab2d9240e7555e4051011c193e
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-04-19 13:54:23 -0500

    Add User bundle

---
+++ src/Beloop/Bundle/UserBundle/BeloopUserBundle.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * This file is part of the Beloop package.
+ *
+ * Copyright (c) 2016 AHDO Studio B.V.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Arkaitz Garro <arkaitz.garro@gmail.com>
+ */
+
+namespace Beloop\Bundle\UserBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+use Beloop\Bundle\CoreBundle\Abstracts\AbstractBundle;
+use Beloop\Bundle\UserBundle\CompilerPass\MappingCompilerPass;
+use Beloop\Bundle\UserBundle\DependencyInjection\BeloopUserExtension;
+
+/**
+ * BeloopUserBundle Bundle.
+ */
+class BeloopUserBundle extends AbstractBundle
+{
+    /**
+     * @param ContainerBuilder $container
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new MappingCompilerPass());
+    }
+
+    /**
+     * Returns the bundle's container extension.
+     *
+     * @return ExtensionInterface The container extension
+     */
+    public function getContainerExtension()
+    {
+        return new BeloopUserExtension();
+    }
+
+    /**
+     * Create instance of current bundle, and return dependent bundle namespaces.
+     *
+     * @param KernelInterface $kernel
+     * @return array Bundle instances
+     */
+    public static function getBundleDependencies(KernelInterface $kernel)
+    {
+        return [
+            'Beloop\Bundle\UserBundle\BeloopUserBundle',
+        ];
+    }
+}
---
+++ src/Beloop/Bundle/UserBundle/DataFixtures/ORM/UserData.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the Beloop package.
+ *
+ * Copyright (c) 2016 AHDO Studio B.V.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Arkaitz Garro <arkaitz.garro@gmail.com>
+ */
+
+namespace Beloop\Bundle\UserBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Beloop\Bundle\CoreBundle\DataFixtures\ORM\Abstracts\AbstractFixture;
+use Beloop\Component\Core\Services\ObjectDirector;
+
+/**
+ * UserData class.
+ *
+ * Load fixtures of user entities
+ */
+class UserData extends AbstractFixture
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        /**
+         * @var ObjectDirector userDirector
+         */
+        $userDirector = $this->getDirector('user');
+
+        /**
+         * User.
+         */
+        $user = $userDirector
+            ->create()
+            ->setPassword('1234')
+            ->setEmail('user@gmail.com');
+
+        $userDirector->save($user);
+        $this->addReference('user', $user);
+    }
+}
