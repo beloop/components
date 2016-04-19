514d3a8fe19cc71ceb5d08b1a8d2f2a070d59f9f
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-04-19 13:54:23 -0500

    Add User bundle

---
+++ DependencyInjection/BeloopUserExtension.php
@@ -0,0 +1,134 @@
+<?php
+
+/*
+ * This file is part of the Beloop package.
+ *
+ * Copyright (c) 2016 AHDO Studio B.V.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Arkaitz Garro <arkaitz.garro@gmail.com>
+ */
+
+namespace Beloop\Bundle\UserBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+use Beloop\Bundle\CoreBundle\DependencyInjection\Abstracts\AbstractExtension;
+use Beloop\Bundle\CoreBundle\DependencyInjection\Interfaces\EntitiesOverridableExtensionInterface;
+
+/**
+ * This class loads and manages your bundle configuration.
+ */
+class BeloopUserExtension extends AbstractExtension implements EntitiesOverridableExtensionInterface
+{
+    /**
+     * @var string
+     *
+     * Extension name
+     */
+    const EXTENSION_NAME = 'beloop_user';
+
+    /**
+     * Get the Config file location.
+     *
+     * @return string Config file location
+     */
+    public function getConfigFilesLocation()
+    {
+        return __DIR__ . '/../Resources/config';
+    }
+
+    /**
+     * Return a new Configuration instance.
+     *
+     * If object returned by this method is an instance of
+     * ConfigurationInterface, extension will use the Configuration to read all
+     * bundle config definitions.
+     *
+     * Also will call getParametrizationValues method to load some config values
+     * to internal parameters.
+     *
+     * @return ConfigurationInterface Configuration file
+     */
+    protected function getConfigurationInstance()
+    {
+        return new Configuration(static::EXTENSION_NAME);
+    }
+
+    /**
+     * Load Parametrization definition.
+     *
+     * return array(
+     *      'parameter1' => $config['parameter1'],
+     *      'parameter2' => $config['parameter2'],
+     *      ...
+     * );
+     *
+     * @param array $config Bundles config values
+     *
+     * @return array Parametrization values
+     */
+    protected function getParametrizationValues(array $config)
+    {
+        return [
+            'beloop.entity.abstract_user.class' => $config['mapping']['abstract_user']['class'],
+            'beloop.entity.abstract_user.mapping_file' => $config['mapping']['abstract_user']['mapping_file'],
+            'beloop.entity.abstract_user.manager' => $config['mapping']['abstract_user']['manager'],
+            'beloop.entity.abstract_user.enabled' => $config['mapping']['abstract_user']['enabled'],
+
+            'beloop.entity.admin_user.class' => $config['mapping']['admin_user']['class'],
+            'beloop.entity.admin_user.mapping_file' => $config['mapping']['admin_user']['mapping_file'],
+            'beloop.entity.admin_user.manager' => $config['mapping']['admin_user']['manager'],
+            'beloop.entity.admin_user.enabled' => $config['mapping']['admin_user']['enabled'],
+
+            'beloop.entity.teacher.class' => $config['mapping']['teacher']['class'],
+            'beloop.entity.teacher.mapping_file' => $config['mapping']['teacher']['mapping_file'],
+            'beloop.entity.teacher.manager' => $config['mapping']['teacher']['manager'],
+            'beloop.entity.teacher.enabled' => $config['mapping']['teacher']['enabled'],
+
+            'beloop.entity.user.class' => $config['mapping']['user']['class'],
+            'beloop.entity.user.mapping_file' => $config['mapping']['user']['mapping_file'],
+            'beloop.entity.user.manager' => $config['mapping']['user']['manager'],
+            'beloop.entity.user.enabled' => $config['mapping']['user']['enabled'],
+        ];
+    }
+
+    /**
+     * Config files to load.
+     *
+     * @param array $config Configuration
+     *
+     * @return array Config files
+     */
+    public function getConfigFiles(array $config)
+    {
+        return [
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getEntitiesOverrides()
+    {
+        return [
+            'Beloop\Component\User\Entity\Interfaces\UserInterface' => 'beloop.entity.user.class',
+            'Beloop\Component\User\Entity\Interfaces\TeacherInterface' => 'beloop.entity.teacher.class',
+            'Beloop\Component\User\Entity\Interfaces\AdminUserInterface' => 'beloop.entity.admin_user.class',
+        ];
+    }
+
+    /**
+     * Returns the extension alias, same value as extension name.
+     *
+     * @return string The alias
+     */
+    public function getAlias()
+    {
+        return static::EXTENSION_NAME;
+    }
+}
---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+ * This file is part of the Beloop package.
+ *
+ * Copyright (c) 2016 AHDO Studio B.V.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Arkaitz Garro <arkaitz.garro@gmail.com>
+ */
+
+namespace Beloop\Bundle\UserBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+use Beloop\Bundle\CoreBundle\DependencyInjection\Abstracts\AbstractConfiguration;
+
+/**
+ * Class Configuration.
+ */
+class Configuration extends AbstractConfiguration
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function setupTree(ArrayNodeDefinition $rootNode)
+    {
+        $rootNode
+            ->children()
+                ->arrayNode('mapping')
+                    ->addDefaultsIfNotSet()
+                    ->children()
+                        ->append($this->addMappingNode(
+                            'abstract_user',
+                            'Beloop\Component\User\Entity\Abstracts\AbstractUser',
+                            '@BeloopUserBundle/Resources/config/doctrine/AbstractUser.orm.yml',
+                            'default',
+                            true
+                        ))
+                        ->append($this->addMappingNode(
+                            'admin_user',
+                            'Beloop\Component\User\Entity\AdminUser',
+                            '@BeloopUserBundle/Resources/config/doctrine/AdminUser.orm.yml',
+                            'default',
+                            true
+                        ))
+                        ->append($this->addMappingNode(
+                            'teacher',
+                            'Beloop\Component\User\Entity\Teacher',
+                            '@BeloopUserBundle/Resources/config/doctrine/Teacher.orm.yml',
+                            'default',
+                            true
+                        ))
+                        ->append($this->addMappingNode(
+                            'user',
+                            'Beloop\Component\User\Entity\User',
+                            '@BeloopUserBundle/Resources/config/doctrine/User.orm.yml',
+                            'default',
+                            true
+                        ))
+                    ->end()
+                ->end()
+            ->end();
+    }
+}
