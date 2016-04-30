d4565e7372157dba406c6969e4a10f93f23ebcc1
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-04-30 23:41:45 +0200

    Factory trait

---
+++ src/Beloop/Component/Core/Factory/Traits/FactoryTrait.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of the Beloop package.
+ *
+ * Copyright (c) 2016 AHDO Studio B.V.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Arkaitz Garro <arkaitz.garro@gmail.com>
+ */
+
+namespace Beloop\Component\Core\Factory\Traits;
+
+/**
+ * Trait FactoryTrait.
+ */
+class FactoryTrait
+{
+    /**
+     * @var \Beloop\Component\Core\Factory\Abstracts\AbstractFactory
+     *
+     * Factory
+     */
+    private $factory;
+
+    /**
+     * Get Factory.
+     *
+     * @return \Beloop\Component\Core\Factory\Abstracts\AbstractFactory Factory
+     */
+    public function getFactory()
+    {
+        return $this->factory;
+    }
+
+    /**
+     * Sets Factory.
+     *
+     * @param \Beloop\Component\Core\Factory\Abstracts\AbstractFactory $factory Factory
+     *
+     * @return $this Self object
+     */
+    public function setFactory(\Beloop\Component\Core\Factory\Abstracts\AbstractFactory $factory)
+    {
+        $this->factory = $factory;
+
+        return $this;
+    }
+}
