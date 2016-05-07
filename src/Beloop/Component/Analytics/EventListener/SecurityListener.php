7a03d7db80fd97169445cec7c6a846ff496d10f4
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-05-07 13:12:39 +0200

    Redirect to login after logout

--- src/Beloop/Component/Analytics/EventListener/SecurityListener.php
+++ src/Beloop/Component/Analytics/EventListener/SecurityListener.php
@@ -17,23 +17,35 @@ namespace Beloop\Component\Analytics\EventListener;
 
 use DateTime;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Router;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
 
 use Beloop\Component\Analytics\Services\AnalyticsManager;
 
 /**
  * Class SecurityListener.
  */
-class SecurityListener
+class SecurityListener implements LogoutSuccessHandlerInterface
 {
     private $analyticsManager;
     private $tokenStorage;
+    private $router;
 
-    public function __construct(AnalyticsManager $analyticsManager, TokenStorageInterface $tokenStorage)
+    public function __construct(
+        AnalyticsManager $analyticsManager,
+        TokenStorageInterface $tokenStorage,
+        Router $router
+    )
     {
         $this->analyticsManager = $analyticsManager;
         $this->tokenStorage = $tokenStorage;
+        $this->router = $router;
     }
 
     public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
@@ -50,5 +62,32 @@ class SecurityListener
             $clientIp,
             new DateTime()
         );
+    }
+
+    /**
+     * Creates a Response object to send upon a successful logout.
+     *
+     * @param Request $request
+     *
+     * @return Response never null
+     */
+    public function onLogoutSuccess(Request $request)
+    {
+        $token = $this->tokenStorage->getToken();
+
+        if ($token instanceof TokenInterface) {
+            $user = $token->getUser();
+            $clientIp = $request->getClientIp();
+
+            $this->analyticsManager->addEntry(
+                $user->getId(),
+                'logout_successful',
+                0,
+                $clientIp,
+                new DateTime()
+            );
+        }
+
+        return new RedirectResponse($this->router->generate('beloop_login'));
     }
 }
