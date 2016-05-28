2501fbd67c63f14e949bf1813e69672a9f90ba6a
Arkaitz Garro <arkaitz.garro@gmail.com>
2016-05-28 11:16:15 +0200

    Fix user and instagram entities

--- src/Beloop/Component/Instagram/Repository/InstagramRepository.php
+++ src/Beloop/Component/Instagram/Repository/InstagramRepository.php
@@ -36,11 +36,10 @@ class InstagramRepository extends EntityRepository
         $qb = $this->createQueryBuilder('i');
 
         $qb
-            ->addSelect('u', 'c')
-                ->innerJoin('i.user', 'u')
-                ->innerJoin('u.courses', 'c')
+            ->addSelect('u, c')
+            ->innerJoin('i.user', 'u')
+            ->innerJoin('u.courses', 'c')
             ->where('c.id IN(:userCourses)')->setParameter('userCourses', $user->getCourses())
-//            ->where('u.courses = :userCourses')->setParameter('userCourses', $user->getCourses())
             ->orderBy('i.createdAt', 'DESC');
         ;
 
@@ -57,22 +56,20 @@ class InstagramRepository extends EntityRepository
      */
     public function findOneBy(array $criteria, array $orderBy = null)
     {
-//        $qb = $this->createQueryBuilder('c');
-//
-//        $qb
-//            ->addSelect('l, m')
-//                ->leftJoin('c.lessons', 'l')
-//                ->leftJoin('l.modules', 'm')
-//            ->orderBy('l.position', 'ASC')
-//                ->addOrderBy('m.position', 'ASC')
-//        ;
-//
-//        foreach ($criteria as $key => $value) {
-//            $qb
-//                ->andWhere(vsprintf('c.%s = :%s', [$key, $key]))
-//                ->setParameter($key, $value);
-//        }
-//
-//        return $qb->getQuery()->getOneOrNullResult();
+        $qb = $this->createQueryBuilder('i');
+
+        $qb
+            ->addSelect('u, c')
+            ->innerJoin('i.user', 'u')
+            ->innerJoin('u.courses', 'c')
+        ;
+
+        foreach ($criteria as $key => $value) {
+            $qb
+                ->andWhere(vsprintf('i.%s = :%s', [$key, $key]))
+                ->setParameter($key, $value);
+        }
+
+        return $qb->getQuery()->getOneOrNullResult();
     }
 }
